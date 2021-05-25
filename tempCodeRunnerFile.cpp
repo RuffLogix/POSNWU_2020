9fb611c3c1b5f4cf211461b1e2829b19ee1ca85e
Teejuta Sriwaranon <67462422+RuffLogix@users.noreply.github.com>
2021-05-25 16:09:44 +0700

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-    if(dist[x.first] > dist[u] + x.second){
-                    if(x.first==d)ans = max(ans,a);  
-
-                    dist[x.first] = dist[u] + x.second;
-                    q.push({dist[x.first],{x.first,min(a,x.second)}});
-                }
